b4f1c4fd828b13a0a019012f4a8cd20f3c1a2dbe
alexshimel91 <alexshimel91@gmail.com>
2017-09-19 00:26:28 +0300

    second edition

--- HomeWork_1/src/Customer.java
+++ HomeWork_1/src/Customer.java
@@ -1,38 +1,24 @@
-public class Customer extends Person implements Employee {
-
-    private String name;
-    private int age;
-    private String work;
-
-    public String resume (String name, int age, String work){
-        return name+" "+age+" "+" "+work;
-    }
-
-    @Override
-    public void pahat() {
-        System.out.println("Hard working or working hard?");
-    }
+public class Customer extends Person implements Working {
 
     @Override
     public String complain(boolean compl) {
-        String complPhrase;
-        if (compl) {
-            System.out.println("one complain");
-            complPhrase = "I need more money, boss";
-        }
-        else {
-            System.out.println("no complains");
-            complPhrase = "I'm happy, boss";
-        }
-        System.out.println(complPhrase);
-        return complPhrase;
+        if (!compl) {
+            System.out.println("do it!");
+        }else{
+            System.out.println("don't do it!"); }
+        return null;
+    }
+    public void toWork(){
+        System.out.println("work!");
+    }
+    @Override
+    public void earn(int a, int b) {
+        double c = Math.pow(a,b);
+        System.out.println(c);
     }
 
     @Override
-    public void earn(int a, int b) {
-        int sum = a+b;
-        System.out.println(sum);
-        int modulo = Math.round(a/b);
-        System.out.println(modulo);
+    public void setupMeeting() {
+        super.setupMeeting();
     }
 }
