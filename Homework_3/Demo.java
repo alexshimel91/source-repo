9286c5606f718341404e34fa998dc80db721c188
alexshimel91 <alexshimel91@gmail.com>
2017-09-27 19:07:51 +0300

    3

---
+++ Homework_3/Demo.java
@@ -0,0 +1,8 @@
+package homework3;
+
+public class Demo {
+    public static void main(String[] args) {
+        WordSize ws = new WordSize();
+        ws.words("Mamma Mamma milalalala ramu ramu");
+    }
+}
